1ec3b60eeb4c825ed2ff3e415259d7e86616bc90
Boudewijn <33985458+bdwjn@users.noreply.github.com>
2022-12-14 03:51:01 +0100

    2022-13: Partial ordering

--- 2022/day13/day13ab.c
+++ 2022/day13/day13ab.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int cmp (const void *_p1, const void *_p2) {
 	const char *p1 = *(const char**)_p1, *p2 = *(const char**)_p2;
@@ -20,12 +19,12 @@ int cmp (const void *_p1, const void *_p2) {
 			if (*p2 == '[') p2++, depth2++;
 			else if (*p2 == ']') return 1;
 			else {
-				int val1 = *p1, val2 = *p2;
+				int num1 = *p1, num2 = *p2;
 
-				if (p1[1]=='0') ++p1, val1 += 9;
-				if (p2[1]=='0') ++p2, val2 += 9;
+				if (p1[1]=='0') ++p1, num1 += 9;
+				if (p2[1]=='0') ++p2, num2 += 9;
 
-				if (val1 == val2) {
+				if (num1 == num2) {
 					while (depth1 && p1[1]==']') p1++, depth1--;
 					if (depth1) return 1;
 					while (depth2 && p2[1]==']') p2++, depth2--;
@@ -34,7 +33,7 @@ int cmp (const void *_p1, const void *_p2) {
 					p1++; p1 += (*p1==',');
 					p2++; p2 += (*p2==',');
 				}
-				else return (val1<val2) ? -1 : 1;
+				else return (num1<num2) ? -1 : 1;
 			}
 		}
 	}
@@ -47,25 +46,23 @@ int main(void)
 
 	char *lines[1000];
 
+	int two_idx = 1, six_idx = 2;
+	const char *two = "[[2]]\n", *six = "[[6]]\n";
+
 	do {
 		lines[n] = malloc(1000);
-		fgets(lines[n++], 1000, stdin);
+		if (!fgets(lines[n++], 1000, stdin)) return 0;
 		lines[n] = malloc(1000);
-		fgets(lines[n++], 1000, stdin);
+		if (!fgets(lines[n++], 1000, stdin)) return 0;
 
 		sum += cmp(lines + (n-2), lines + (n-1)) == -1 ? idx : 0;
+
+		if (cmp(lines + (n-2), &two) < 0) two_idx++, six_idx++;
+		else if (cmp(lines + (n-2), &six) < 0) six_idx ++;
+		if (cmp(lines + (n-1), &two) < 0) two_idx++, six_idx++;
+		else if (cmp(lines + (n-1), &six) < 0) six_idx ++;
 	} while (idx++, getchar() != EOF);
 
-	lines[n++] = "[[2]]\n";
-	lines[n++] = "[[6]]\n";
-
-	qsort(lines, n, sizeof(char*), cmp);
-
-	for (int i=0; i<n; i++)
-	{
-		if (!strcmp(lines[i], "[[2]]\n") || !strcmp(lines[i], "[[6]]\n")) mul *= (i+1);
-	}
-
 	printf("Part 1: %d\n", sum);
-	printf("Part 2: %d\n", mul);
+	printf("Part 2: %d\n", two_idx * six_idx);
 }
