5700443afb0e630e151fb703dbe555f55d7180a8
Boudewijn <33985458+bdwjn@users.noreply.github.com>
2022-12-26 00:44:42 +0100

    Added 2022-25

---
+++ 2022/day25/day25a.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+unsigned short lut[] = { 0xFF00, 0xFF01, 0xFF02, 0x00FE, 0x00FF, 0, 0x0001, 0x0002, 0x01FE, 0x01FF, 0x0100 };
+
+int main(void) {
+	signed char data[4096] = { '\n' }, sum[30] = { 0 }, *p = data + 1, *end, v, *sp, carry;
+
+	if (!fread(data+1, 1, 4095, stdin)) return -1;
+
+	do {
+		do; while (*++p != '\n');
+		end = p;
+
+		sp = sum + sizeof sum - 1;
+
+		p--;
+		while (*p != '\n') {
+			v = "432567merryxmas!3"[*p - '-'] - '0';
+			p--;
+
+			perform_carry: {}
+			int l = lut[*sp + v + carry];
+			*sp-- = l;
+			carry = l >> 8;
+		}
+		if (carry) { v=5; goto perform_carry; }
+
+		p = end + 1;
+	} while (*p);
+
+	for (int i=0, nz=0; i<30; i++) {
+		nz |= sum[i] && putchar("=-012"[sum[i]+2]);
+	}
+}
